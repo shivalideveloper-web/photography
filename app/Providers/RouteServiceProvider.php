ff5e8eaf5832609bc4e25f0fe12c2324d97b3e78
shivali <>
2026-04-21 18:03:48 +0530

    chnages

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    protected $namespace = '\App\Http\Controllers';
+    protected $namespace = 'App\Http\Controllers';
     public const HOME = '/home';
 
 
